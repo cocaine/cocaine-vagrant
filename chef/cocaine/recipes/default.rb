4b5a34e637bbd29295794e23c3b10968841229d3
Evgeny Safronov <division494@gmail.com>
2013-08-11 23:52:50 +0400

    Tornado proxy is also installed.

--- chef/cocaine/recipes/default.rb
+++ chef/cocaine/recipes/default.rb
@@ -15,3 +15,24 @@ python_pip "cocaine" do
   version "0.10.6.5"
   action :install
 end
+
+
+# Install tornado-proxy
+include_recipe "git"
+
+git "/vagrant/cocaine-tornado-proxy" do
+  repository "https://github.com/noxiouz/cocaine-TornadoProxy.git"
+  reference "master"
+  action :sync
+end
+
+bash "install cocaine-tornado-proxy" do
+  user "root"
+  cwd "/vagrant/cocaine-tornado-proxy"
+  code <<-EOH
+  /usr/bin/python setup.py install
+  EOH
+end
+
+
+
