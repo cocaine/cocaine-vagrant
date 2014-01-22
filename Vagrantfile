dde4f82970aa655e502757914a376747741c344a
Anton Tyurin <noxiouz@yandex.ru>
2014-01-22 17:02:42 +0400

    Introduce VM with cocaine v0.11

--- Vagrantfile
+++ Vagrantfile
@@ -5,13 +5,21 @@ Vagrant.configure("2") do |config|
   config.omnibus.chef_version = "11.6.0"
 
   config.vm.define "cocaine-install"
-  config.vm.box = "precise64"
-  config.vm.box_url = "http://files.vagrantup.com/precise64.box"
 
-  config.vm.network :forwarded_port, guest: 80, host: 48080
+  # plain precise64 box with kernel upgraded to 3.8
+  # and GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"
+  config.vm.box = "precise64-docker"
+  config.vm.box_url = "http://repo.reverbrain.com/vagrant-base/precise64-docker.box"
 
+#  config.vm.network :forwarded_port, guest: 80, host: 48080
+  config.vm.network "private_network", ip: "10.11.12.13"
+  
   config.berkshelf.enabled = true
 
+  config.vm.provider "virtualbox" do |v|
+    v.memory = 1024
+  end
+  
   config.vm.provision :chef_solo do |chef|
     chef.add_recipe 'apt::default'
     chef.add_recipe 'git'
@@ -19,3 +27,4 @@ Vagrant.configure("2") do |config|
     chef.add_recipe 'cocaine'
   end
 end
+
--- chef/cocaine/recipes/default.rb
+++ chef/cocaine/recipes/default.rb
@@ -1,39 +1,87 @@
+
 include_recipe "python"
 
-apt_repository "cocaine" do
-  uri "http://ppa.launchpad.net/reverbrain/testing/ubuntu"
-  distribution node["lsb"]["codename"]
-  components ["main"]
-  keyserver "keyserver.ubuntu.com"
-  key "73F7B2D9"
-end
+package "curl"
 
-package "libcocaine-core2"
-package "cocaine-runtime"
-
-python_pip "cocaine" do
-  version "0.10.6.6"
-  action :install
-end
-
-##### Examples #####
-python_pip "pillow"
-python_pip "qrcode"
-
-bash "configure proxy" do
-  user "root"
-  cwd "/vagrant/examples"
+bash "Turn on reverbrain repo" do
   code <<-EOH
-  mkdir /etc/cocaine
-  cp cocaine-tornado-proxy.conf /etc/cocaine/cocaine-tornado-proxy.conf
+cp -v /vagrant/chef/cocaine/repo.reverbrain.conf /etc/apt/sources.list.d/reverbrain.list
+
+curl http://repo.reverbrain.com/REVERBRAIN.GPG | apt-key add -
+
+apt-get update
+EOH
+end
+
+package "g++"
+package "python-dev"
+
+bash "cocaine packages" do
+  code <<-EOH
+apt-get install -y\
+  libcocaine-core2 \
+  cocaine-runtime \
+  cocaine-framework-native \
+  libcocaine-plugin-chrono2 \
+  libcocaine-plugin-cache2 \
+  libcocaine-plugin-docker2 \
+  libcocaine-dev
+EOH
+end
+
+python_pip "cocaine"
+python_pip "cocaine-tools"
+
+bash "install docker" do
+  creates "/etc/docker.chef.done"
+  user "root"
+  cwd "/"
+  code <<-EOH
+# Add the repository to your APT sources
+echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list
+# Then import the repository key
+curl http://get.docker.io/gpg | apt-key add -
+# Install docker
+apt-get update ; apt-get install -y lxc-docker
+usermod -aG docker vagrant
+usermod -aG docker cocaine
+
+touch /etc/docker.chef.done
+
+
   EOH
 end
 
-bash "install QR example" do
+
+bash "configure cocaine-runtime and proxy" do
   user "root"
-  cwd "/vagrant/examples/qr"
+  cwd "/vagrant/examples"
   code <<-EOH
-  cocaine-tool app upload
+if [ ! -f /etc/cocaine/cocaine.conf ]; then
+  mkdir -p /etc/cocaine
+  cp /vagrant/examples/cocaine-tornado-proxy.conf /etc/cocaine/cocaine-tornado-proxy.conf
+
+cat <<EOF > /etc/default/cocaine-runtime
+CONFIG_PATH="/etc/cocaine/cocaine.conf"
+RUNTIME_PATH="/var/run/cocaine"
+EOF
+
+  cp /vagrant/cocaine.conf /etc/cocaine/cocaine.conf
+
+  service cocaine-runtime restart
+
+fi
+  
+  EOH
+end
+
+bash "create cocaine app control objects" do
+  code <<-EOH
+  
+  cocaine-tool runlist list | grep -q default || {
+    cocaine-tool runlist create --name default
+    cocaine-tool profile upload --name default --profile='{"pool-limit": 4, "isolate": { "type": "docker" }}'
+  }
   EOH
 end
 
@@ -41,8 +89,7 @@ bash "bootstrap" do
   user "root"
   cwd "/"
   code <<-EOH
-  cocaine-tool profile upload --name default --profile='{"pool-limit": 4}'
-  cocaine-tool app start --name qr --profile default
   cocaine-tool proxy start --daemon --port=80
+
   EOH
 end
